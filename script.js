0f1fca48bf27089217c94b076c4110f378efb607
Brendan Morgan <bmo_32@hotmail.com>
2023-03-08 09:57:29 -0500

    add functionality to modal to allow it to open and close when user clicks specific button

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
         
         
         const grid = document.querySelector('.grid-container');
-        grid.setAttribute('style', 'border: solid black 2px; height: 512px; width: 512px; grid-template-columns: repeat(16, 32px);')
-
+        grid.setAttribute('style', 'border: solid black 1px; height: 512px; width: 512px; grid-template-columns: repeat(16, 32px);')
+        
+        //this function creates each individual pixel that will collectively constitute the grid in its entirety
         function buildBox() {
 
             const box = document.createElement('div');
             box.classList.add('box');
-            box.setAttribute('style', 'border: solid green 2px;');
+            box.setAttribute('style', 'border: solid black 1px;');
             grid.appendChild(box);
+
+            grid.addEventListener('mousedown', function (e) {
+                e.target.style.background = 'black';
+              });
+            
         }
-    
-        
-        function createGrid(x) {
 
-    
+        function createGrid(x,y) {
 
-                for (let column = 0; column < x; column++){
+                let gridUnits = x*y;
+
+                for (let currentGridUnit = 0; currentGridUnit < gridUnits; currentGridUnit++){
 
                     buildBox();
-                    console.log(x)
-
-
-                }
-                //grid.setAttribute('style', 'grid-template-columns : repeat(16, 32px);')
-                //grid.setAttribute('style','grid-template-columns:repeat(4, 100px);')
-                
+                }     
         }
 
-        createGrid(256);
+        createGrid(16,16);
+
+        const modal = document.querySelector('.modal');
+        const overlay = document.querySelector('.overlay');
+        const openModalBtn = document.querySelector('.btn-open');
+        const closeModalBtn = document.querySelector('.btn-close');
+
+        const openModal = function () {
+
+            modal.classList.remove('hidden');
+            overlay.classList.remove('hidden');
+        }
+
+        const closeModal = function () {
+
+            modal.classList.add('hidden');
+            overlay.classList.add('hidden');
+        }
+
+
+        openModalBtn.addEventListener("click", openModal);
+        closeModalBtn.addEventListener("click", closeModal);
+        overlay.addEventListener("click", closeModal);
