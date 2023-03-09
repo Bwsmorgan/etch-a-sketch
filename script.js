06f3b407a5a8746a14193afb8c7c1c6ed4a32edb
Brendan Morgan <bmo_32@hotmail.com>
2023-03-08 21:49:24 -0500

    add reset function to reset grid when new grid dimensions are entered by user

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
         
         
         const grid = document.querySelector('.grid-container');
-        grid.setAttribute('style', 'border: solid black 1px; height: 512px; width: 512px; grid-template-columns: repeat(16, 32px);')
+        grid.setAttribute('style', 'border: solid black 1px; height: 512px; width: 512px;');
+        const box = document.createElement('div');
         
         //this function creates each individual pixel that will collectively constitute the grid in its entirety
-        function buildBox() {
+        function buildBox(x,y) {
+            //grid.setAttribute('style', `border: solid black 1px; height: 512px; width: 512px; grid-template-columns: repeat(${x}, ${y});`)
 
             const box = document.createElement('div');
             box.classList.add('box');
             box.setAttribute('style', 'border: solid black 1px;');
             grid.appendChild(box);
+
+            grid.setAttribute('style', `border: solid black 1px; height: 512px; width: 512px; grid-template-columns: repeat(${y}, ${x}`+ `px);`)
 
             grid.addEventListener('mousedown', function (e) {
                 e.target.style.background = 'black';
@@ -22,47 +26,59 @@
         const overlay = document.querySelector('.overlay');
         const openModalBtn = document.querySelector('.btn-open');
         const closeModalBtn = document.querySelector('.btn-close');
-        const submitBtn = document.querySelector('.btn-submit');
+        //const submitBtn = document.querySelector('.btn-submit');
 
         const openModal = function () {
 
             modal.classList.remove('hidden');
             overlay.classList.remove('hidden');
+
         }
 
         const closeModal = function () {
 
             modal.classList.add('hidden');
             overlay.classList.add('hidden');
+            
         }
+
+        function reset() {
+            document
+              .querySelectorAll(".box")
+              .forEach((e) => e.parentNode.removeChild(e));
+          }
 
 
         openModalBtn.addEventListener("click", openModal);
         closeModalBtn.addEventListener("click", closeModal);
         overlay.addEventListener("click", closeModal);
 
-        //const gridLength = document.getElementById('length').value;
-        //const gridWidth = document.getElementById('width').value;
+        function getVals() {
 
-        function getVal(){
+            reset();
+            //once submit is clicked it should remove the old grid then close the modal
 
-            closeModal();
-            
+            closeModal();            
+
             const gridLength = document.getElementById('length').value;
             const gridWidth = document.getElementById('width').value;
-            console.log(gridLength)
-            console.log(gridWidth)
+
+            createGrid(gridLength,gridWidth);
         }
 
 
-        function createGrid(x,y) {
+        function createGrid(length,width) {
 
-            let gridUnits = x*y;
+            let gridUnits = length*width;
+            let unitSize = 512/length;
 
             for (let currentGridUnit = 0; currentGridUnit < gridUnits; currentGridUnit++){
 
-                buildBox();
+                buildBox(unitSize,length);
             }     
+
+            document.getElementById('length').value = "";
+            document.getElementById('width').value = "";
         }
 
         createGrid(16,16);
